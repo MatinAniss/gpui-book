362ee144a6dd523c0e562b608c1dee1b0f95e472
Matin Aniss <76515905+MatinAniss@users.noreply.github.com>
2025-06-27 22:44:54 +1000

    Utilise empty element

--- src/architecture/snippets/mouse_position.rs
+++ src/architecture/snippets/mouse_position.rs
@@ -1,10 +1,10 @@
-use gpui::{AppContext, Application, Context, IntoElement, Render, Window, WindowOptions, div};
+use gpui::{AppContext, Application, Context, Empty, IntoElement, Render, Window, WindowOptions};
 
 struct RootView;
 
 impl Render for RootView {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
-        div()
+        Empty
     }
 }
 
